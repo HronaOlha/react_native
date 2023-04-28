32f68cd7de469668adf12838b84086344ba79385
Грона Оля <grona.hell@gmail.com>
2023-04-28 17:31:46 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  Button,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      {/* <StatusBar style="auto" /> */}
+      <ImageBackground
+        style={styles.img}
+        source={require("./assets/images/bg-image.jpg")}
+      >
+        <View style={styles.form}>
+          <Text style={styles.inputTitle}>Реєстрація</Text>
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="Логін"
+              placeholderTextColor="#BDBDBD"
+            />
+          </View>
+          <View style={{ marginTop: 16 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Адреса електронної пошти"
+              placeholderTextColor="#BDBDBD"
+            />
+          </View>
+          <View style={{ marginTop: 16 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Пароль"
+              placeholderTextColor="#BDBDBD"
+              secureTextEntry={true}
+            />
+          </View>
+          <Button title="Зареєструватися" style={styles.button} />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +51,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  img: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    // alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    backgroundColor: "#F6F6F6",
+    height: 50,
+    // color: "#BDBDBD",
+    padding: 16,
+    fontSize: 16,
+  },
+  form: {
+    marginHorizontal: 16,
+  },
+  inputTitle: {
+    marginBottom: 33,
+    fontWeight: 500,
+    fontSize: 30,
+    textAlign: "center",
+  },
+  button: {
+    marginTop: 43,
+    // padding: "16 32",
+    paddingVertical: 50,
+    paddingHorizontal: 32,
+    // height: 51,
+    borderRadius: 4,
   },
 });
