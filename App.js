b43c4f4a929132ec65ef81b9fed12ec6e42ca2c8
Грона Оля <grona.hell@gmail.com>
2023-07-16 04:55:30 +0300

    remade everything

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-import React from "react";
-import {} from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-
-import useRoute from "./router";
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import { Main } from './components/Main';
 
 export default function App() {
-  const routing = useRoute(null);
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <>
-      <NavigationContainer>{routing}</NavigationContainer>
+      <StatusBar style="auto" />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Main />
+        </PersistGate>
+      </Provider>
     </>
   );
 }
